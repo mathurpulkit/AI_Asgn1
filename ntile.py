f68fd80789116efb1e90709005da2f533aa02997
mathurpulkit <pmathur2001@gmail.com>
2022-08-12 22:13:14 +0530

    Created the basic ntile Board class, defined the moves, created a func which will create a duplicate and perform moves on it. created a function which determines if a board is solved.

---
+++ ntile.py
@@ -0,0 +1,63 @@
+# defines the general class of a board with n*n tiles
+# we will use 0 to denote empty tile
+import copy
+
+class ntile:
+    def __init__(self,n: int, initial_state: ntile = None):
+        self.n = n
+        if initial_state is None:
+            self.board = [[i for i in range(self.n)] for j in range(self.n)]
+            for i in range(1,self.n):
+                for j in range(self.n):
+                    self.board[i][j] += i*self.n
+            self.board[self.n-1][self.n-1] = 0
+        else:
+            self.board = copy.deepcopy(initial_state.state)
+        return
+
+    def move(self, move: str):
+        # convention is that we are moving the blank space
+        # returns False if no valid move is performed, else returns True
+        # find 0 in the board
+        for i in range(self.n):
+            for j in range(self.n):
+                if self.board[i][j] == 0:
+                    zero_i = i
+                    zero_j = j
+                    break
+        if move == 'U':
+            if zero_i == 0:
+                return False
+            self.board[zero_i][zero_j], self.board[zero_i-1][zero_j] = self.board[zero_i-1][zero_j], self.board[zero_i][zero_j]
+            return True
+        elif move == 'D':
+            if zero_i == self.n-1:
+                return False
+            self.board[zero_i][zero_j], self.board[zero_i+1][zero_j] = self.board[zero_i+1][zero_j], self.board[zero_i][zero_j]
+            return True
+        elif move == 'L':
+            if zero_j == 0:
+                return False
+            self.board[zero_i][zero_j], self.board[zero_i][zero_j-1] = self.board[zero_i][zero_j-1], self.board[zero_i][zero_j]
+            return True
+        elif move == 'R':
+            if zero_j == self.n-1:
+                return False
+            self.board[zero_i][zero_j], self.board[zero_i][zero_j+1] = self.board[zero_i][zero_j+1], self.board[zero_i][zero_j]
+            return True
+        return False
+
+    def newBoardMove(self, move: str):
+        # creates a duplicate of the board and performs the move on the duplicate, returns the duplicate
+        new_board = ntile(self.n, self.board)
+        new_board.move(move)
+        return new_board
+
+    def is_solved(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                if self.board[i][j] != i*self.n + j + 1 or (i == self.n-1 and j == self.n-1 and self.board[i][j] == 0):
+                    return False
+        return True
+
+    
