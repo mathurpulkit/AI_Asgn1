96e63d28aea3f7b312d289babdf6d1d512f13b45
mathurpulkit <pmathur2001@gmail.com>
2022-08-27 16:56:53 +0530

    modified and added test cases in main_bidir

--- main_bidir.py
+++ main_bidir.py
@@ -2,10 +2,10 @@
 import bidir
 import ntile
 import time
-initial_state = [[1,8,2],
+initial_game = [[1,8,2],
                  [0,4,3],
                  [7,6,5]]
-game1 = ntile.ntile(3, initial_state)
+game1 = ntile.ntile(3, initial_game)
 
 four_by_four = [[5,1,2,3],
                 [9,6,7,4], 
@@ -13,11 +13,18 @@ four_by_four = [[5,1,2,3],
                 [14,15,0,12]]
 game2 = ntile.ntile(4, four_by_four)
 
-third_ex = [[15,2,1,12],
+five_by_five = [[1,0,2,4,5],
+                [6,7,3,8,10],
+                [11,12,9,13,15],
+                [16,17,18,14,19],
+                [21,22,23,24,20]]
+game3 = ntile.ntile(5, five_by_five)
+
+tough_ex = [[15,2,1,12],
             [8,5,6,11],
             [4,9,10,7],
             [3,4,13,0]]
-game3 = ntile.ntile(4, third_ex)
+game4 = ntile.ntile(4, tough_ex)
 
 tic = time.time()
 ans = bidir.bidirectional_search(game1)
@@ -31,3 +38,7 @@ tic = time.time()
 ans3 = bidir.bidirectional_search(game3)
 toc = time.time()
 print(ans3, "\ntime:", toc-tic)
+tic = time.time()
+ans4 = bidir.bidirectional_search(game4)
+toc = time.time()
+print(ans4, "\ntime:", toc-tic)
